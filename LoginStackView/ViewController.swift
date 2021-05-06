8b18df09c1a8902da8312163e135d1f5d81f3c62
mrlandfried <justin.landfried@desertsands.us>
2021-05-06 11:42:45 -0700

    Initial Commit

---
+++ LoginStackView/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  LoginStackView
+//
+//  Created by Justin Landfried on 1/25/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
